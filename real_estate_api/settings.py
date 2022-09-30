5dfe68b4e0dbbf8fb89cb2327eb0d29badc4f6c5
Anthony Oliko <tobeoliko@gmail.com>
2022-09-30 11:57:45 +0100

    db

--- real_estate_api/settings.py
+++ real_estate_api/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'real_estate_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'CkqoGNKivu7u3BKqgHvj',
-        'HOST': 'containers-us-west-28.railway.app',
-        'PORT': '6446',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
